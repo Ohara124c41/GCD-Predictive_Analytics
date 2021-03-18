b12b635f4bcccf3eba1baf0eeb4e9e3d6498baa5
Christopher Ohara <c.a.ohara@student.tue.nl>
2021-03-18 23:27:16 +0100

    feat: hookspecs quant and qual ilities; TODO: rename functions

---
+++ plugin_architecture/gcd/hookspecs.py
@@ -0,0 +1,21 @@
+import pluggy
+
+hookspec = pluggy.HookspecMarker("gcd")
+
+
+@hookspec
+def gcd_pair_ilities(quant: tuple):
+    """Have a look at the pairings and edit.
+
+    :param scores: the pairing, don't touch them!
+    :return: a list of ility pairings
+    """
+
+
+@hookspec
+def gcd_ilities_score(ilities: dict):
+    """Reorganize the ilities tray to your heart's content.
+
+    :param ilities: some QAs and stuff
+    :return: a witty comment about your activity
+    """
