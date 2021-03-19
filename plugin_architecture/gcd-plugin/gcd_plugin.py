f8abd9f0c70e27637023cb6c444571efb0652e2d
Christopher Ohara <c.a.ohara@student.tue.nl>
2021-03-19 15:11:03 +0100

    feat: create three simple plugin examples (mutable objects, return function result)

---
+++ plugin_architecture/gcd-plugin/gcd_plugin.py
@@ -0,0 +1,36 @@
+import gcd
+
+
+@gcd.hookimpl
+def gcd_pair_ilities(quant):
+    """Here the caller expects us to return a list."""
+    if "Cost" in quant:
+        feedback = ["Budget-Derived", "Manage-Budget"]
+    else:
+        feedback = ["Needs-Financial-Analysis", "Do-Feasibility-Study"]
+    return feedback
+
+
+@gcd.hookimpl
+def gcd_ilities_score(ilities):
+    """Here the caller passes a mutable object, so we mess with it directly."""
+    try:
+        del ilities["Sustainability"]
+    except KeyError:
+        pass
+    ilities["Extensibility"] = 42
+    return "This looks like a system engineering problem..."
+
+@gcd.hookimpl
+def gcd_average_score(quant):
+    """Here the caller expects us to return a list."""
+    if "Performance" in quant:
+        designer1_score = 5
+        designer2_score = 4
+        designer3_score = 4
+        average_score = (designer1_score + designer2_score + designer3_score)/3
+        feedback = print("The average score for Performance is {}" .format(average_score))
+
+#    else:
+#        feedback = ["Do-Performance-Scoring", "Performance-Not-Scored"]
+    return feedback
