44b7e610167ef9734f06944d99e15d417ee1fe84
Christopher Ohara <c.a.ohara@student.tue.nl>
2021-03-21 00:51:01 +0100

    feat: add gcd_average_score hookspec

--- plugin_architecture/gcd/hookspecs.py
+++ plugin_architecture/gcd/hookspecs.py
@@ -6,7 +6,6 @@ hookspec = pluggy.HookspecMarker("gcd")
 @hookspec
 def gcd_pair_ilities(quant: tuple):
     """Have a look at the pairings and edit.
-
     :param scores: the pairing, don't touch them!
     :return: a list of ility pairings
     """
@@ -15,7 +14,13 @@ def gcd_pair_ilities(quant: tuple):
 @hookspec
 def gcd_ilities_score(ilities: dict):
     """Reorganize the ilities tray to your heart's content.
+    :param ilities: some QAs and stuff
+    :return: a witty comment about your activity
+    """
 
+@hookspec
+def gcd_average_score(score: dict):
+    """Reorganize the ilities tray to your heart's content.
     :param ilities: some QAs and stuff
     :return: a witty comment about your activity
     """
