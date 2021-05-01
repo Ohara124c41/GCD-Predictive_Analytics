10d095ccc19727b052b955885a8f2f10ed1cf372
Christopher Ohara <c.a.ohara@student.tue.nl>
2021-05-01 21:49:07 +0200

    refactor: cleaning up

--- stakeholder_agents/arch01/delta_metadata.py
+++ stakeholder_agents/arch01/delta_metadata.py
@@ -1,6 +1,5 @@
 import random
-
-
+import json
 
 json_object["id"] = "001"
 json_object["labels"] = "Architecture"
@@ -9,53 +8,43 @@ y1 = random.randint(1, 5)
 y2 = random.randint(1, 5)
 y3 = random.randint(1, 5)
 
-
-json2 = {'synergy': y1, 'usability': y2, 'maturity': y3}
-json_object["agent2-qual-metrics"] = json2
+auto_qual_scores = {'synergy': y1, 'usability': y2, 'maturity': y3}
+json_object["agent2-qual-metrics"] = auto_qual_scores
 
 ## Design Decisions for synergy
 if y1 >= 4:  
   json_object["DD1"] = "synergy is good"
-
-if y1 <= 2:
+elif y1 <= 2:
   json_object["DD1"] = "synergy is bad"
-
-if y1 == 3:
+else:
   json_object["DD1"] = "synergy is nominal"
 
 ## Design Decisions for usability
-
 if y2 >= 4:  
   json_object["DD2"] = "usability is good"
-
-if y2 <= 2:
+elif y2 <= 2:
   json_object["DD2"] = "usability is bad"
-
-if y2 == 3:
+else:
   json_object["DD2"] = "usability is nominal"
 
 ## Design Decisions for maturity
-
 if y3 >= 4:  
   json_object["DD3"] = "maturity is good"
-
-if y3 <= 2:
+elif y3 <= 2:
   json_object["DD3"] = "maturity is bad"
-
-if y3 == 3:
+else:
   json_object["DD3"] = "maturity is nominal"
 
 ## Pseudo assessment criteria
+score = y1 + y2 + y3
 
-if y1 + y2 + y3 > 12:
-  json_object["Assessment"] = "This architecture is excellent"
+if score > 12:
+  json_object["Assessment"] = "This architecture is excellent, the SCORE is %d" % (score)
+elif score < 6:
+  json_object["Assessment"] = "This architecture is unacceptable, the SCORE is %d" % (score)
+else:
+  json_object["Assessment"] = "This architecture is considerable, the SCORE is %d" % (score)
 
-if y1 + y2 + y3 < 6:
-  json_object["Assessment"] = "This architecture is unacceptable"
-
-if 12 > y1 + y2 + y3 > 6:
-  json_object["Assessment"] = "This architecture is unacceptable"
-
-a_file = open("designer01.json", "w")
+a_file = open("ARCH01.json", "w")
 json.dump(json_object, a_file)
 a_file.close()
